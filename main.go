a080f8d7aa3d179eab678c6db4dfb1f34e53a0ad
Alex <little_al14@hotmail.com>
2017-11-18 21:51:24 -0400

    shutdown hook added

--- main.go
+++ main.go
@@ -4,17 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"github.com/Alexander1994/golite/database"
 )
 
 func main() {
+	// Setup
 	reader := bufio.NewReader(os.Stdin)
 	var cmd string
 	cmds := createCmds()
 	database.OpenDB()
 
+	// Shut down hook
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		args := make([]string, 1)
+		cmds[":exit"](args)
+	}()
+
+	// Main loop
 	for true {
 		print("golite>")
 		cmd, _ = reader.ReadString('\n') // second var is error
